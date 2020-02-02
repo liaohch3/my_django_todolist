6b20a226b15efdbf088599d3bb2c84ed8cc1e6b8
liaohch3 <liaohch3@mail2.sysu.edu.cn>
2020-02-02 20:46:51 +0800

    finish CURD

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: liaohch3
 # @Date:   2020-02-02 00:12:48
-# @Last Modified by:   10151
-# @Last Modified time: 2020-02-02 12:34:19
+# @Last Modified by:   liaohch3
+# @Last Modified time: 2020-02-02 19:16:12
 
 from django.urls import path
 from . import views 
@@ -11,5 +11,8 @@ app_name = 'todolist'
 urlpatterns = [
     path('home/', views.home, name="主页"),
     path('about/', views.about, name="关于"),
-    path('edit/', views.edit, name="编辑"),
+    path('edit/<forloop_counter>', views.edit, name="编辑"),
+    path('delete/<forloop_counter>', views.delete, name="删除"),
+    path('finish/<forloop_counter>', views.finish, name="划掉"),
+    path('unfinish/<forloop_counter>', views.unfinish, name="撤销"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+lst = [
+	{'待办事项': '遛狗', '完成情况': False},
+	{'待办事项': '拖地', '完成情况': False},
+	{'待办事项': '拖地', '完成情况': True},
+	{'待办事项': '拖地', '完成情况': False},
+]
 
 # Create your views here.
 def home(request):
-	return render(request, 'todolist/home.html')
+	global lst
+
+	if request.method == "POST":
+		if request.POST.get('添加的待办事项') == '':
+			content = {'警告': '待办事项不能为空！', '清单': lst}
+			return render(request, 'todolist/home.html', content)
+		else:
+			lst.append({'待办事项': request.POST.get('添加的待办事项'), '完成情况': False})
+			content = {'清单': lst}
+			return render(request, 'todolist/home.html', content)
+	else:
+		content = {'清单': lst}
+		return render(request, 'todolist/home.html', content)
+
+def delete(request, forloop_counter):
+	global lst
+
+	lst.pop(int(forloop_counter) - 1)
+	return redirect('todolist:主页')
+
+def finish(request, forloop_counter):
+	global lst
+
+	lst[int(forloop_counter) - 1]['完成情况'] = True
+	return redirect('todolist:主页')
+
+def unfinish(request, forloop_counter):
+	global lst
+
+	lst[int(forloop_counter) - 1]['完成情况'] = False
+	return redirect('todolist:主页')
 
 def about(request):
 	return render(request, 'todolist/about.html')
 
-def edit(request):
-	return render(request, 'todolist/edit.html')
+def edit(request, forloop_counter):
+	global lst
+
+	if request.method == "POST":
+		if request.POST.get('编辑后的待办事项') == '':
+			content = {'警告': '待办事项不能为空！'}
+			return render(request, 'todolist/edit.html', content)
+		else:
+			# lst.append({'待办事项': request.POST.get('编辑后的待办事项'), '完成情况': False})
+			lst[int(forloop_counter)-1]['待办事项'] = request.POST.get('编辑后的待办事项')
+			content = {'清单': lst}
+			return redirect('todolist:主页')
+	else:
+		content = {'待修改事项': lst[int(forloop_counter)-1]['待办事项']}
+		return render(request, 'todolist/edit.html', content)
